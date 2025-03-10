ba750cd94a1f8d6bd596d35a6b4f8f68b8b2ae0e
Enzo Jose <enzojose.reus@gmail.com>
2025-03-10 10:01:08 -0300

    feat: add default connections to dbUI

--- lua/enzo-jose/lazy/plugins/dadbod.lua
+++ lua/enzo-jose/lazy/plugins/dadbod.lua
@@ -13,7 +13,12 @@ return {
         })
 
         vim.g.db_ui_use_nerd_fonts = 1;
-        vim.keymap.set("n", "<leader>dB", "<Cmd>:tabnew+DBUIToggle<CR>", {desc = "Open [DB] UI"});
-        vim.keymap.set("n", "<leader>db", "<Cmd>:DBUIToggle<CR>", {desc = "Open [DB] UI"});
+        vim.g.dbs = {
+            { name = 'eloverde_env_dev',  url = "mysql://enzo:" .. vim.fn.getenv("MY_SQL_DB_PASSWORD") .. "@localhost:3306/eloverde_env_dev" },
+            { name = 'eloverde_env_prod', url = "mysql://enzo:" .. vim.fn.getenv("MY_SQL_DB_PASSWORD") .. "@localhost:3306/eloverde_env_prod" },
+        }
+
+        vim.keymap.set("n", "<leader>dB", "<Cmd>:tabnew+DBUIToggle<CR>", { desc = "Open [DB] UI" });
+        vim.keymap.set("n", "<leader>db", "<Cmd>:DBUIToggle<CR>", { desc = "Open [DB] UI" });
     end
 }
