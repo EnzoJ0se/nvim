b11d329d0374ac8d47df63e849199352e02dad78
Enzo Jose <enzojose.reus@gmail.com>
2025-08-26 12:18:22 -0300

    feat: auto load dbs from my local mysql

--- lua/enzo-jose/lazy/plugins/dadbod.lua
+++ lua/enzo-jose/lazy/plugins/dadbod.lua
@@ -1,24 +1,35 @@
+local function buildDBS()
+    local dbs = {}
+    local user = vim.fn.getenv("MY_SQL_DB_USER")
+    local pass = vim.fn.getenv("MY_SQL_DB_PASSWORD_ENCODED")
+    local cmd = "get-db-schemas " .. user .. ' "' .. vim.fn.getenv("MY_SQL_DB_PASSWORD") .. '"'
+    local output = vim.fn.systemlist(cmd)
+
+    for i = 2, #output do
+        local schema = output[i]
+        table.insert(dbs, {name = schema, url = string.format("mysql://%s:%s@localhost:3306/%s", user, pass, schema)})
+    end
+
+    return dbs
+end
+
 return {
     "tpope/vim-dadbod",
     dependencies = {
-        {"kristijanhusak/vim-dadbod-completion",  ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
+        { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
         "kristijanhusak/vim-dadbod-ui",
     },
     config = function()
-        vim.g.db_ui_use_nerd_fonts = 1;
-        vim.g.dbs = {
-            { name = 'eloverde_env_dev',  url = "mysql://enzo:" .. vim.fn.getenv("MY_SQL_DB_PASSWORD") .. "@localhost:3306/eloverde_env_dev" },
-            { name = 'eloverde_env_prod', url = "mysql://enzo:" .. vim.fn.getenv("MY_SQL_DB_PASSWORD") .. "@localhost:3306/eloverde_env_prod" },
-            { name = 'api_eloverde_integrations', url = "mysql://enzo:" .. vim.fn.getenv("MY_SQL_DB_PASSWORD") .. "@localhost:3306/api_eloverde_integrations" },
-        }
+        local view = require("enzo-jose.utils.view-helper")
 
-        local view = require('enzo-jose.utils.view-helper');
-
-        vim.keymap.set("n", "<leader>db", "<Cmd>:DBUIToggle<CR>", { desc = "Open [DB] UI" });
-        vim.keymap.set('n', '<leader>dB', function() view:openFloatingWindow({ commands = "zsh -i -c 'nvim +DBUIToggle'" }) end, {
-            desc = "Open [DB] UI",
-            noremap = true,
-            silent = true,
-        });
-    end
+        vim.g.db_ui_use_nerd_fonts = 1
+        vim.g.dbs = buildDBS()
+        vim.keymap.set("n", "<leader>db", "<Cmd>:DBUIToggle<CR>", { desc = "Open [DB] UI" })
+        vim.keymap.set(
+            "n",
+            "<leader>dB",
+            function() view:openFloatingWindow({ commands = "zsh -i -c 'nvim +DBUIToggle'" }) end,
+            {desc = "Open [DB] UI", noremap = true, silent = true}
+        )
+    end,
 }
