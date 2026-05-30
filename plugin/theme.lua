5e418607736d79e7b2f718ce0dea452d5c446b47
Enzo Jose <enzojose.reus@gmail.com>
2026-05-30 11:17:13 -0300

    refactor: add missing pluggins on migration

--- plugin/theme.lua
+++ plugin/theme.lua
@@ -7,63 +7,43 @@ do
     -- tokyonight-night, tokyonight-storm, tokyonight-day, tokyonight-moon
     vim.cmd.colorscheme("tokyonight-night")
 end
--- {
---     "kaicataldo/material.vim",
---     lazy = false,
---     priority = 1000,
---     config = function()
---         -- 'default' | 'palenight' | 'ocean' | 'lighter' | 'darker'
---         -- 'default-community' | 'palenight-community' | 'ocean-community' | 'lighter-community' | 'darker-community'
---         vim.g.material_theme_style = 'darker'
---         vim.cmd.colorscheme "material"
---     end,
--- },
--- {
---     "catppuccin/nvim",
---     name = "catppuccin",
---     priority = 1000,
---     config = function()
---         -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
---         vim.cmd.colorscheme "catppuccin-macchiato"
---     end,
--- },
--- {
---     "dracula/vim",
---     lazy = false,
---     priority = 1000,
---     name = "dracula",
---     opts = {},
---     config = function()
---         vim.cmd.colorscheme "dracula"
---     end,
--- },
--- {
---     "folke/tokyonight.nvim",
---     lazy = false,
---     priority = 1000,
---     opts = {},
---     config = function()
---         -- tokyonight-night, tokyonight-storm, tokyonight-day, tokyonight-moon
---         vim.cmd.colorscheme "tokyonight-night"
---     end,
--- },
--- {
---     'navarasu/onedark.nvim',
---     priority = 1000,
---     config = function() vim.cmd.colorscheme 'onedark' end,
--- },
--- {
---     "oxfist/night-owl.nvim",
---     lazy = false,
---     priority = 1000,
---     config = function()
---         -- load the colorscheme here
---         vim.cmd.colorscheme "night-owl"
---     end,
--- },
--- {
---     'AlexvZyl/nordic.nvim',
---     lazy = false,
---     priority = 1000,
---     config = function() vim.cmd.colorscheme 'nordic' end
--- },
+-- do
+--     vim.pack.add({ gh("kaicataldo/material.vim") })
+--     -- 'default' | 'palenight' | 'ocean' | 'lighter' | 'darker'
+--     -- 'default-community' | 'palenight-community' | 'ocean-community' | 'lighter-community' | 'darker-community'
+--     vim.g.material_theme_style = "darker"
+--     vim.cmd.colorscheme("material")
+-- end
+--
+-- do
+--     vim.pack.add({ gh("catppuccin/nvim") })
+--     -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
+--     vim.cmd.colorscheme("catppuccin-macchiato")
+-- end
+--
+-- do
+--     vim.pack.add({ gh("dracula/vim") })
+--     vim.cmd.colorscheme("dracula")
+-- end
+--
+-- do
+--     vim.pack.add({ gh("folke/tokyonight.nvim") })
+--     -- tokyonight-night, tokyonight-storm, tokyonight-day, tokyonight-moon
+--     vim.cmd.colorscheme("tokyonight-night")
+-- end
+--
+-- do
+--     vim.pack.add({ gh("navarasu/onedark.nvim") })
+--     vim.cmd.colorscheme("onedark")
+-- end
+--
+-- do
+--     vim.pack.add({ gh("oxfist/night-owl.nvim") })
+--     -- load the colorscheme here
+--     vim.cmd.colorscheme("night-owl")
+-- end
+--
+-- do
+--     vim.pack.add({ gh("AlexvZyl/nordic.nvim") })
+--     vim.cmd.colorscheme("nordic")
+-- end
--- plugin/trouble.lua
+++ plugin/trouble.lua
@@ -1,38 +1,15 @@
--- return {
---     "folke/trouble.nvim",
---     dependencies = { "nvim-tree/nvim-web-devicons" },
---     opts = {},
---     cmd = "Trouble",
---     keys = {
---         {
---             "<leader>td",
---             "<cmd>Trouble diagnostics toggle<cr>",
---             desc = "Diagnostics (Trouble)",
---         },
---         {
---             "<leader>tD",
---             "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
---             desc = "Buffer Diagnostics (Trouble)",
---         },
---         {
---             "<leader>ts",
---             "<cmd>Trouble symbols toggle focus=false<cr>",
---             desc = "Symbols (Trouble)",
---         },
---         {
---             "<leader>tt",
---             "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
---             desc = "LSP Definitions / references / ... (Trouble)",
---         },
---         {
---             "<leader>tl",
---             "<cmd>Trouble loclist toggle<cr>",
---             desc = "Location List (Trouble)",
---         },
---         {
---             "<leader>tq",
---             "<cmd>Trouble qflist toggle<cr>",
---             desc = "Quickfix List (Trouble)",
---         },
---     },
--- }
+do
+    vim.pack.add({
+        { src = gh("nvim-tree/nvim-web-devicons") },
+        { src = gh("folke/trouble.nvim") },
+    })
+
+    require("trouble").setup({})
+
+    vim.keymap.set("n", "<leader>td", "<cmd>Trouble diagnostics toggle<cr>", { desc = "Diagnostics (Trouble)" })
+    vim.keymap.set("n", "<leader>tD", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", { desc = "Buffer Diagnostics (Trouble)" })
+    vim.keymap.set("n", "<leader>ts", "<cmd>Trouble symbols toggle focus=false<cr>", { desc = "Symbols (Trouble)" })
+    vim.keymap.set("n", "<leader>tt", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", { desc = "LSP Definitions / references / ... (Trouble)" })
+    vim.keymap.set("n", "<leader>tl", "<cmd>Trouble loclist toggle<cr>", { desc = "Location List (Trouble)" })
+    vim.keymap.set("n", "<leader>tq", "<cmd>Trouble qflist toggle<cr>", { desc = "Quickfix List (Trouble)" })
+end
